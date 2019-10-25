ace75dd798d20f98e44f3882cca0cbdf8a2265b8
ashish1sasmal <ashishsasmal1@gmail.com>
2019-10-25 15:07:57 +0530

    read write show images

---
+++ read_write_show_image.py
@@ -0,0 +1,19 @@
+import cv2
+img=cv2.imread('dan.jpg',-1)  # imread --> read the image 
+						 # ('path to image',flag=1(color) or 
+						 # 0(grayscale) or -1(alpha channel))
+
+print(img)  # img stores a matrix
+
+cv2.imshow('image',img)  # imshow()  --> display the image
+						 # ('name of window' ,variable value(matrix form))
+
+k = cv2.waitKey(0)		#  waitkey()  --> time limit of image displayed
+						# key value get stored in k variable
+
+if k==27:				# if the key is 'ESC' button
+	cv2.destroyAllWindows()
+elif ord('s'):           #if ctrl+s is pressed
+
+	cv2.imwrite('dan_copy.png',img)
+	cv2.destroyAllWindows()
