29ec2752c8bc9eab97d870e9a6cea6e0f148064e
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-04 03:42:15 +0530

    canny+live cam

--- canny_edge.py
+++ canny_edge.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from live_cam import live
 
 def nothing():
     pass
@@ -8,14 +9,14 @@ def nothing():
 cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
 
 
-img = cv2.imread("dan.jpg")
+
 
 cv2.createTrackbar('Min','Image',1,1000,nothing)
 
 cv2.createTrackbar('Max','Image',1,1000,nothing)
 
 while (1):
-
+    img = live()
     m =  cv2.getTrackbarPos('Min','Image')
 
     M =  cv2.getTrackbarPos('Max','Image')
@@ -23,7 +24,8 @@ while (1):
     dst = cv2.Canny(img,m,M)
     cv2.imshow("Image",dst)
 
-    k = cv2.waitKey(1) & 0xFF
+    k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+        
 cv2.destroyAllWindows()
