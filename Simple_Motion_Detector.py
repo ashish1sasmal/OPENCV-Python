c0f62d22cae903ddf1dedd3a2dc50aa9dde2ab43
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-12 05:54:26 +0530

    changes

--- Simple_Motion_Detector.py
+++ Simple_Motion_Detector.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 import time
 import datetime
+from live_cam import live
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v","--video",help="Path to video")
@@ -14,6 +16,7 @@ args = vars(ap.parse_args())
 
 if args.get("video",None) is None:
     vs = VideoStream(src=0).start()
+    # vs = live()
     time.sleep(2.0)
 else:
     vs = cv2.VideoCapture(args["video"])
@@ -21,6 +24,7 @@ else:
 firstFrame = None
 
 while True:
+    # frame = live()
     frame = vs.read()
     frame = frame if args.get("video",None) is None else frame[1]
     text = "Unoccupied"
@@ -42,17 +46,19 @@ while True:
     thresh = cv2.dilate(thresh, None, iterations=2)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-
+    # print(len(cnts))
+    n=0
     for c in cnts:
+
         if cv2.contourArea(c) < args["area"]:
             continue
-
+        n+=1
         (x,y,w,h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x,y) ,(x+w,y+h), (0,255,0), 2)
         text = "Occupied"
 
     cv2.putText(frame, f"Room Status:{text}",(10,20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    cv2.putText(frame,f"Total Objects : {n} "+ datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
     cv2.imshow("Security Feed", frame)
     cv2.imshow("Thresh",thresh)
