2b8b4f956da8f2693c4d96af3b00b7d7950ace80
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-28 22:43:56 +0530

    MRI images

--- Template Matching/template_matching.py
+++ Template Matching/template_matching.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+import sys
 
 #
-# cv2.namedWindow("TM",cv2.WINDOW_NORMAL)
-img = cv2.imread("Brain Tumors/tumor2.jpg",0)
+cv2.namedWindow("MRI",cv2.WINDOW_NORMAL)
+img = cv2.imread(f"Brain Tumors/{sys.argv[1]}",0)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 img = clahe.apply(img)
-templ = cv2.imread("Brain Tumors/test_brain.png",0)
+templ = cv2.imread("Brain Tumors/ref_brain.png",0)
 templ = clahe.apply(templ)
 print(50/templ.shape[::-1][0], 64/templ.shape[::-1][1])
-templ = templ[74:138, 70:120]
+temp = templ[74:138, 70:120]
 
-templ = cv2.resize(templ,(0,0), fx=(img.shape[::-1][0]*50)/templ.shape[::-1][0] , fy=(img.shape[::-1][1]*64)/templ.shape[::-1][1])
+templ = cv2.resize(temp, (int((img.shape[::-1][0]*50)/templ.shape[::-1][0]) , int((img.shape[::-1][1]*64)/templ.shape[::-1][1])), interpolation = cv2.INTER_CUBIC)
 
 w, h =templ.shape[::-1]
 
@@ -31,14 +33,17 @@ top_left = max_loc
 print(top_left)
 bottom_right = (max_loc[0]+w, max_loc[1]+h)
 
-cv2.rectangle(out, top_left, bottom_right, 255, 2)
+cv2.rectangle(img, top_left, bottom_right, (0,0,255) , 2)
 
-plt.subplot(121),plt.imshow(res,cmap = 'gray')
-plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(out,cmap = 'gray')
-plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-plt.suptitle(methods[0])
-plt.show()
+# plt.subplot(121),plt.imshow(res)
+# plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(out)
+# plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+# plt.suptitle(methods[0])
+# plt.show()
+
+cv2.imshow("MRI",img)
+cv2.waitKey(0)
 
 # 70, 74
 # 120, 138
