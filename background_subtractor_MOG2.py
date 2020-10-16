589accc3c16eb4bf515ba85ee46ae6fdc2347bc6
Ashish Sasmal <ashishsasmal1@gmail.com>
2020-10-16 22:42:09 +0530

    new video

--- background_subtractor_MOG2.py
+++ background_subtractor_MOG2.py
@@ -1,3 +1,10 @@
+# @Author: ASHISH SASMAL <ashish>
+# @Date:   01-09-2020
+# @Last modified by:   ashish
+# @Last modified time: 16-10-2020
+
+
+
 import cv2
 import numpy as np
 
@@ -5,7 +12,7 @@ from live_cam import live
 
 cv2.namedWindow("MOG",cv2.WINDOW_NORMAL)
 
-cap = cv2.VideoCapture("car2.mp4")
+cap = cv2.VideoCapture("boy_ball.mp4")
 
 
 fgbg = cv2.createBackgroundSubtractorMOG2(history=20)
@@ -13,10 +20,12 @@ fgbg = cv2.createBackgroundSubtractorMOG2(history=20)
 while True:
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
-
+    kernel = np.ones((4,4), np.uint8)
+    ers = cv2.erode(fgmask,kernel,iterations=1)
+    fgmask = cv2.resize(ers, (720,360))
     cv2.imshow("MOG",fgmask)
 
-    k = cv2.waitKey(1) & 0xff
+    k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
